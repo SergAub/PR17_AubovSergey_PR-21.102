84e7c202c347637fff8f21e4a2bce5f382ea0f86
Sergey <sergeypanz355@mail.ru>
2023-10-01 19:12:41 +0700

    Код адаптирован к работе с 4 строками

--- app/src/main/java/com/example/pr17_aubovsergey_pr_21102/MainActivity.java
+++ app/src/main/java/com/example/pr17_aubovsergey_pr_21102/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.pr17_aubovsergey_pr_21102;
 
-import static androidx.core.content.PackageManagerCompat.LOG_TAG;
-
+import static androidx.core.content.ContextCompat.getSystemService;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -12,34 +11,73 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
 
     DBHelper dbHelper;
+    EditText etAnimal, etName, etSize, etHeight;
+    Button btnInsert, btnRead, btnClear;
 
-    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         dbHelper = new DBHelper(this);
+        etAnimal = findViewById(R.id.etAnimal);
+        etName = findViewById(R.id.etName);
+        etSize = findViewById(R.id.etSize);
+        etHeight = findViewById(R.id.etHeight);
+        btnInsert = findViewById(R.id.btnInsert);
+        btnClear = findViewById(R.id.btnClear);
+        btnRead = findViewById(R.id.btnRead);
 
-        // создаемобъектдляданных
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertData();
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readData();
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearData();
+            }
+        });
+    }
+
+    public void insertData() {
+        // создаем объект для данных
         ContentValues cv = new ContentValues();
 
-        // получаемданныеизполейввода
-        String animal = etName.getText().toString();
-        String name = etEmail.getText().toString();
-        double size = etSize.getText().toString();
-        double height = etHeight.getText().toString();
+        // получаем данные из полей ввода
+        String animal = etAnimal.getText().toString();
+        String name = etName.getText().toString();
+        double size, height;
+        try {
+            size = Double.parseDouble(etSize.getText().toString());
+            height = Double.parseDouble(etHeight.getText().toString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-        // подключаемсякБД
+
+        // подключаемся к БД
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         // подготовим данные для вставки в виде пар: наименование столбца - значение
-
         cv.put("animal", animal);
         cv.put("name", name);
         cv.put("size", size);
@@ -47,65 +85,74 @@ public class MainActivity extends AppCompatActivity {
 
         // вставляем запись и получаем ее ID
         long rowID = db.insert("mytable", null, cv);
-        Log.d(LOG_TAG, "row inserted, ID = "+ rowID);
-        break;
-        caseR.id.btnRead:
-        Log.d(LOG_TAG, "--- Rows in mytable: ---");
+        Log.d("LOG_TAG", "row inserted, ID = " + rowID);
+    }
 
+    public void readData() {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Log.d("LOG_TAG", "--- Rows in mytable: ---");
         // делаем запрос всех данных из таблицы mytable, получаем Cursor
         Cursor c = db.query("mytable", null, null, null, null, null, null);
-
         // ставим позицию курсора на первую строку выборки
         // если в выборке нет строк, вернется false
         if(c.moveToFirst()) {
-
             // определяем номера столбцов по имени в выборке
             int idColIndex = c.getColumnIndex("id");
+            int animalColIndex = c.getColumnIndex("animal");
             int nameColIndex = c.getColumnIndex("name");
-            int emailColIndex = c.getColumnIndex("email");
+            int sizeColIndex = c.getColumnIndex("size");
+            int heightColIndex = c.getColumnIndex("height");
 
-            do{
+            do {
                 // получаем значения по номерам столбцов и пишем все в лог
-                Log.d(LOG_TAG,
+                Log.d("LOG_TAG",
                         "ID = "+ c.getInt(idColIndex) +
+                                ", animal = "+ c.getString(animalColIndex) +
                                 ", name = "+ c.getString(nameColIndex) +
-                                ", email = "+ c.getString(emailColIndex));
+                                ", size = "+ c.getDouble(sizeColIndex) +
+                                ", height = "+ c.getDouble(heightColIndex));
                 // переход на следующую строку
                 // а если следующей нет (текущая - последняя), то false - выходим из цикла
             } while(c.moveToNext());
         } else
-            Log.d(LOG_TAG, "0 rows");
+            Log.d("LOG_TAG", "0 rows");
         c.close();
-        break;
-        caseR.id.btnClear:
-        Log.d(LOG_TAG, "--- Clear mytable: ---");
-        // удаляемвсезаписи
+    }
+
+    public void clearData() {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Log.d("LOG_TAG", "--- Clear mytable: ---");
+        // удаляем все записи
         int clearCount = db.delete("mytable", null, null);
-        Log.d(LOG_TAG, "deleted rows count = "+ clearCount);
-        break;
-    }
-    // закрываем подключение к БД
-    dbHelper.close();
-
-}
-class DBHelper extends SQLiteOpenHelper {
-
-    void publicDBHelper(Context context) {
-        // конструкторсуперкласса
-        super(context, "myDB", null, 1);
-    }
-
-    void publicvoidonCreate(SQLiteDatabasedb) {
-        Log.d(LOG_TAG, "--- onCreate database ---");
-        // создаемтаблицусполями
-        db.execSQL("create table mytable ("
-                + "id integer primary key autoincrement,"
-                + "name text,"
-                + "email text"+ ");");
+        Log.d("LOG_TAG", "deleted rows count = " + clearCount);
     }
 
     @Override
-    publicvoidonUpgrade(SQLiteDatabasedb, intoldVersion, intnewVersion) {
+    protected void onDestroy() {
+        dbHelper.close();
+        super.onDestroy();
+    }
+}
+
+
+class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context) {
+        super(context, "myDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE mytable ("
+                + "id INTEGER PRIMARY KEY, "
+                + "animal TEXT, "
+                + "name TEXT, "
+                + "size REAL, "
+                + "height REAL" + ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
 }
